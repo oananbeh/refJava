7e68fad1699503e9597b14df3c29d302bcce0ca8
obieda ananbeh <oannbeh@outlook.com>
2023-05-24 23:28:33 -0400

    add java files

---
+++ src/main/java/com/refJava/refJava/RefJavaApplication.java
@@ -0,0 +1,13 @@
+package com.refJava.refJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/refJava/refJava/RefJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refJava.refJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
